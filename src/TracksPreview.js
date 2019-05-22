0beeeb2aadfb347d9062f2c8674b4b70e8eaa669
James Kong <jameskong@airfyer.local>
2019-05-22 06:00:38 -0400

    Added semantic-ui-react styling.

--- src/TracksPreview.js
+++ src/TracksPreview.js
@@ -1,28 +1,57 @@
 import React from 'react'
+import { Segment, Table } from 'semantic-ui-react'
 
 class TracksPreview extends React.Component {
-    render() {
+    renderContent() {
         if(!this.props.tracks) {
-            return <p>Loading...</p>;
+            return (
+                <Segment>
+                    Loading...
+                </Segment>
+            );
         }
         if(this.props.tracks.length > 0) {
             return (
-                <div>
-                    {this.props.tracks.map(
-                        track => <div>{track.title} - {track.artist}</div>
-                    )}
-                </div>
-            );
+                <Table unstackable compact selectable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Title</Table.HeaderCell>
+                            <Table.HeaderCell>Artist</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {this.props.tracks.map(track =>
+                            <Table.Row key={track.title + track.artist}>
+                                <Table.Cell>
+                                    {track.title}
+                                </Table.Cell>
+                                <Table.Cell>
+                                    {track.artist}
+                                </Table.Cell>
+                            </Table.Row>
+                        )}
+                    </Table.Body>
+                </Table>
+            )
         }
         return (
-            <div>
+            <Segment>
                 <p>No tracks to display.</p>
                 <p>Suggestions</p>
                 <ul>
                     <li>Add more Source playlists</li>
                     <li>Change or remove Filter playlists</li>
                 </ul>
-            </div>
+            </Segment>
+        );
+    }
+
+    render() {
+        return (
+            <Segment>
+                <h3>Tracks Preview</h3>
+                {this.renderContent()}
+            </Segment>
         );
     }
 }
