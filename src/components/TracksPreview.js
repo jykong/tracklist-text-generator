d82f5346c1643aee37030bd282a120de4a650ba5
James Kong <jameskong@airfyer.local>
2019-06-02 16:59:47 -0400

    Minor style changes to TracksPreview.js

--- src/components/TracksPreview.js
+++ src/components/TracksPreview.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Segment, Table, Icon, Button } from 'semantic-ui-react'
 
 class TracksPreview extends React.Component {
+    onRemoveTrack = (e, {value}) => {
+        this.props.onRemoveTrack(value)
+    }
+
     renderArtists(artists) {
         let s = ''
         for(let i = 0; ; i++) {
@@ -13,8 +17,63 @@ class TracksPreview extends React.Component {
         }
     }
 
-    onRemoveTrack = (e, {value}) => {
-        this.props.onRemoveTrack(value)
+    renderTable() {
+        const headerCellStyle = {padding: '.5em .5em'};
+        const cellStyle = {padding: '.3em .5em'};
+        const buttonStyle = {margin: 0};
+
+        return (
+            <Table unstackable compact selectable
+                style={{border: 'none', 
+                    borderTop: '1px solid rgba(34,36,38,.15)',
+                    margin: 0
+                }}
+            >
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell width={8} style={headerCellStyle}>
+                            Title
+                        </Table.HeaderCell>
+                        <Table.HeaderCell width={7} style={headerCellStyle}>
+                            Artist
+                        </Table.HeaderCell>
+                        <Table.HeaderCell width={1} style={headerCellStyle}>
+                            <Button compact basic onClick={this.props.onClearTracks}
+                                style={buttonStyle}
+                            >
+                                Clear
+                            </Button>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {this.props.tracks.map(track =>
+                        <Table.Row key={track.id}>
+                            <Table.Cell width={8}  style={cellStyle}>
+                                {track.title}
+                            </Table.Cell >
+                            <Table.Cell width={7}  style={cellStyle}>
+                                {this.renderArtists(track.artists)}
+                            </Table.Cell>
+                            <Table.Cell width={1} textAlign='right'  style={cellStyle}>
+                                <Button
+                                    icon basic compact
+                                    onClick={this.onRemoveTrack}
+                                    value={track.id}
+                                    style={buttonStyle}
+                                >
+                                    <Icon
+                                        name='remove'
+                                        color='grey'
+                                        link
+                                    />
+                                </Button>
+                            </Table.Cell>
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table>
+        )
     }
 
     renderContents() {
@@ -26,48 +85,7 @@ class TracksPreview extends React.Component {
             );
         }
         if(this.props.tracks.length > 0) {
-            return (
-                <Table unstackable compact selectable>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell width={8}>Title</Table.HeaderCell>
-                            <Table.HeaderCell width={7}>Artist</Table.HeaderCell>
-                            <Table.HeaderCell width={1}>
-                                <Button compact basic onClick={this.props.onClearTracks}>
-                                    Clear
-                                </Button>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {this.props.tracks.map(track =>
-                            <Table.Row key={track.id}>
-                                <Table.Cell width={8}>
-                                    {track.title}
-                                </Table.Cell >
-                                <Table.Cell width={7}>
-                                    {this.renderArtists(track.artists)}
-                                </Table.Cell>
-                                <Table.Cell width={1} textAlign='right'>
-                                    <Button
-                                        icon 
-                                        basic
-                                        compact
-                                        onClick={this.onRemoveTrack}
-                                        value={track.id}
-                                    >
-                                        <Icon
-                                            name='remove'
-                                            color='grey'
-                                            link
-                                        />
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )}
-                    </Table.Body>
-                </Table>
-            )
+            return this.renderTable();
         }
         return (
             <Segment>
